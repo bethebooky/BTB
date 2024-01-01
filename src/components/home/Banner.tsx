a001b4dd416a8128a6b0eb2978ddd52c8c945ef2
Mohit Chandel <devmohit93@gmail.com>
2024-01-02 00:20:54 +0530

    solved bg

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -152,8 +152,7 @@ const Banner = () => {
     <section
       className="banner-area banner-bg"
       style={{
-        backgroundImage:
-          "url(/assets/img/banner/banner_p.png?v=${new Date().getTime()})",
+        background: "url(/assets/img/banner/banner_p.png",
       }}
     >
       <div className="container">
